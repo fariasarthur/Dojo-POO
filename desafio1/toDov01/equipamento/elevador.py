fbc1ffe3de396305d91926e05030c3386e29c1f7
fariasarthur <arthur.fariasrc@gmail.com>
2024-01-31 17:52:16 -0300

    first commit

---
+++ desafio1/toDov01/equipamento/elevador.py
@@ -0,0 +1,27 @@
+
+class Elevador:
+
+    def __init__(self, andar_atual:int, andar_desejado:int):
+        self.num_andar = andar
+        self.pavimentos = [1,2,3,4,5,6,8,9,10,11,12,13,14]
+
+    def chamar(self):
+        print('Aguarde um momento')
+        print('..................')
+        print('  Porta Fechada'   )
+        print('       -||-       ')
+        print('  Porta Aberta    ')
+        print('-|              -|')
+        print('Escolha um andar')
+    
+    def escolher_andar(self):
+
+    def transitar(self):
+
+        for pavimento in self.pavimentos:
+
+            if andar 
+
+        
+    
+        
